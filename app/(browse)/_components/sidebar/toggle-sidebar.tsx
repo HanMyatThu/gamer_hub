53a2b7104ff18836fcd937487aab5030cde0d644
Han Myat Thu <hanmyatthu111@gmail.com>
2024-08-12 00:00:49 +0200

    create tooltip hint component for sidebar

--- app/(browse)/_components/sidebar/toggle-sidebar.tsx
+++ app/(browse)/_components/sidebar/toggle-sidebar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useSidebar } from "@/store/use-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { ToolTipHint } from "@/components/common/tooltip-hint";
+import { useSidebar } from "@/store/use-sidebar";
 
 export const ToggleSidebar = () => {
   const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
@@ -14,26 +16,30 @@ export const ToggleSidebar = () => {
       {!collapsed && (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
-          <Button
-            onClick={onCollapse}
-            className="h-auto p-2 ml-auto"
-            size="sm"
-            variant="ghost"
-          >
-            <ArrowLeftFromLine className="size-4" />
-          </Button>
+          <ToolTipHint label={label} side="right" asChild>
+            <Button
+              onClick={onCollapse}
+              className="h-auto p-2 ml-auto"
+              size="sm"
+              variant="ghost"
+            >
+              <ArrowLeftFromLine className="size-4" />
+            </Button>
+          </ToolTipHint>
         </div>
       )}
       {collapsed && (
         <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4 ">
-          <Button
-            onClick={onExpand}
-            className="h-auto p-2"
-            size="sm"
-            variant="ghost"
-          >
-            <ArrowRightFromLine className="size-4" />
-          </Button>
+          <ToolTipHint label={label} side="right" asChild>
+            <Button
+              onClick={onExpand}
+              className="h-auto p-2"
+              size="sm"
+              variant="ghost"
+            >
+              <ArrowRightFromLine className="size-4" />
+            </Button>
+          </ToolTipHint>
         </div>
       )}
     </>
